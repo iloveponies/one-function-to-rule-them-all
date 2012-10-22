110a809cef364ec5c5020d8bc9eb8fb0f818fa8e
Jani Rahkola <jani.rahkola@gmail.com>
2012-10-22 20:28:02 +0300

    Added the exercises from the material.

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,1 +1,22 @@
 (ns one-function-to-rule-them-all)
+
+(defn concat-elements [a-seq]
+  [:-])
+
+(defn my-count [a-seq]
+  :-)
+
+(defn str-cat [a-seq]
+  :-)
+
+(defn my-reverse [a-seq]
+  [:-])
+
+(defn min-max-element [a-seq]
+  [:-])
+
+(defn insert [sorted-seq n]
+  [:-])
+
+(defn insertion-sort [a-seq]
+  [:-])
--- test/one_function_to_rule_them_all_test.clj
+++ test/one_function_to_rule_them_all_test.clj
@@ -1,3 +1,37 @@
 (ns one-function-to-rule-them-all-test
   (:use midje.sweet
         one-function-to-rule-them-all))
+
+(facts "concat-elements"
+       (concat-elements [])            => (just '())
+       (concat-elements [[1 2]])       => (just '(1 2))
+       (concat-elements [[1 2] [3 4]]) => (just '(1 2 3 4)))
+
+(facts "my-count"
+       (my-count [])      => 0
+       (my-count [1 2 3]) => 3
+       (my-count [1])     => 1)
+
+(facts "str-cat"
+       (str-cat ["I" "am" "Legend"]) => "I am Legend"
+       (str-cat ["I" "am" "back"])   => "I am back"
+       (str-cat ["more" " " "space"]) => "more   space")
+
+(facts "my-reverse"
+       (my-reverse [1 2 3]) => (just '(3 2 1))
+       (my-reverse [1 2])   => (just '(2 1))
+       (my-reverse [])      => (just '()))
+
+(facts "min-max-element"
+       (min-max-element [2 7 3 15 4]) => (just '(2 15))
+       (min-max-element [1 2 3 4])    => (just '(1 4))
+       (min-max-element [1])          => (just '(1 1)))
+
+(facts "insert"
+       (insert [] 2)      => (just '(2))
+       (insert [1 3 4] 2) => (just '(1 2 3 4))
+       (insert [1] 2)     => (just '(1 2)))
+
+(facts "insertion-sort"
+       (insertion-sort [2 5 3 1]) => (just '(1 2 3 5))
+       (insertion-sort [1 2])     => (just '(1 2)))
