9b4974c108cf160a41faac7ee2acd923f70c3a14
Kalle J Lammenoja <lammenoj@dhp-20.cs.helsinki.fi>
2012-10-25 16:24:58 +0300

    meh

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,25 +1,37 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if(empty? a-seq)
+    ""
+    (reduce (fn [acc x] (str acc " " x)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (let [f (fn [acc a] (conj acc x a))]
+    (rest(reduce f [] a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [acc a] (inc acc)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn[acc a] (cons a acc)) [] a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn[[x y] a][(min x a) (max y a)]) [(first a-seq) (first a-seq)] (rest a-seq)))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (loop [p []
+         r sorted-seq
+         n n]
+    (cond
+      (empty? r)
+        (conj p n)
+      (<= n (first r))
+        (concat p [n] r)
+      :else 
+        (recur (conj p (first r)) (rest r) n))))
 
 (defn insertion-sort [a-seq]
   [:-])
