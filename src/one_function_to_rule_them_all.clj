f5d5e4ace4fe9ae9d0b1a36e348d9438401e1b35
Kalle J Lammenoja <lammenoj@dhp-20.cs.helsinki.fi>
2012-10-26 13:41:29 +0300

    done

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -37,19 +37,34 @@
   (reduce insert '() a-seq))
 
 (defn parity [a-seq]
-  [:-])
+    (reduce (fn [acc a] (if (contains? acc a)
+                          (disj acc a)
+                          (conj acc a))) #{} a-seq))
 
-(defn minus [x]
-  )
+(defn minus
+  ([x]   (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params 
+  ([& more] (my-count more)))
 
-(defn my-* [x]
-  :-)
+(defn my-* 
+  ([] 1)
+  ([& more] (let[rec (fn rec [coll](if (empty? coll)
+   								 		1
+    							 (* (first coll) (rec (rest coll)))))]
+              (rec more))))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([] (fn [x] true))
+  ([p] p)
+  ([p & more] (fn [x] (reduce (fn [acc a] (and acc (a x))) (p x) more))))
 
-(defn my-map [f a-seq]
-  [:-])
+(defn my-map [f & a-seq]
+  (cond
+    (some empty? a-seq)
+      []
+    :else 
+      (cons 
+       (apply f (map first a-seq)) 
+       (apply (partial my-map f ) (map rest a-seq)))))
