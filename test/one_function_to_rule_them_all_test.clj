6d307840b3426d515bfb29530ee75c1cafc4f1a7
Jani Rahkola <jani.rahkola@gmail.com>
2012-11-25 18:39:59 +0200

    Some tests.

--- test/one_function_to_rule_them_all_test.clj
+++ test/one_function_to_rule_them_all_test.clj
@@ -5,7 +5,8 @@
 (facts "concat-elements"
        (concat-elements [])            => (just '())
        (concat-elements [[1 2]])       => (just '(1 2))
-       (concat-elements [[1 2] [3 4]]) => (just '(1 2 3 4)))
+       (concat-elements [[1 2] [3 4]]) => (just '(1 2 3 4))
+       (concat-elements [[1 2 2] [2 3]]) => (just '(1 2 2 2 3)))
 
 (facts "str-cat"
        (str-cat ["I" "am" "Legend"])  => "I am Legend"
@@ -14,21 +15,25 @@
        (str-cat [])                   => "")
 
 (facts "my-interpose"
-       (my-interpose 0 [1 2 3])               => [1 0 2 0 3]
-       (my-interpose "," ["I" "me" "myself"])
-       => (just '("I" "," "me" "," "myself"))
-       (my-interpose :a [1])                  => [1]
-       (my-interpose :a [])                   => [])
+  (my-interpose 0 [1 2 3])
+  => [1 0 2 0 3]
+  (my-interpose "," ["I" "me" "myself"])
+  => (just '("I" "," "me" "," "myself"))
+  (my-interpose :a [1])
+  => [1]
+  (my-interpose :a [])
+  => [])
 
 (facts "my-count"
-       (my-count [])      => 0
-       (my-count [1 2 3]) => 3
-       (my-count [1])     => 1)
+  (my-count [])      => 0
+  (my-count [1 2 3]) => 3
+  (my-count [1])     => 1)
 
 (facts "my-reverse"
-       (my-reverse [1 2 3]) => (just '(3 2 1))
-       (my-reverse [1 2])   => (just '(2 1))
-       (my-reverse [])      => (just '()))
+  (my-reverse [1 2 3])    => (just '(3 2 1))
+  (my-reverse [1 2 2 3 3] => (just '(3 3 2 2 1)))
+  (my-reverse [1 2])      => (just '(2 1))
+  (my-reverse [])         => (just '()))
 
 (facts "min-max-element"
        (min-max-element [2 7 3 15 4]) => [2 15]
@@ -38,11 +43,13 @@
 (facts "insert"
        (insert [] 2)      => (just '(2))
        (insert [1 3 4] 2) => (just '(1 2 3 4))
+       (insert [3 5 7] 2) => (just '(2 3 5 7))
        (insert [1] 2)     => (just '(1 2)))
 
 (facts "insertion-sort"
-       (insertion-sort [2 5 3 1]) => (just '(1 2 3 5))
-       (insertion-sort [1 2])     => (just '(1 2)))
+  (insertion-sort [2 5 3 1])             => (just '(1 2 3 5))
+  (insertion-sort (shuffle (range 100))) => (range 100)
+  (insertion-sort [1 2])                 => (just '(1 2)))
 
 (facts "parity"
        (parity [:a :b :c])    => #{:a :b :c}
