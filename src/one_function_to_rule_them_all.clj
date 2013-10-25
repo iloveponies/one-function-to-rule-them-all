2e446c10386499bd52bdf5eb2b3b401b868c5692
Teemu Patja <tp@iki.fi>
2013-10-25 09:52:44 +0100

    22/46

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,22 +1,27 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq) )
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) ""
+      (reduce str (interpose " " a-seq))))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (<= (count a-seq) 1) a-seq
+      (reduce (fn [m n] (flatten [m x n])) a-seq)))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [a _] (inc a)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce conj '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  [
+   (reduce (fn [a b] (if (< a b) a b)) a-seq)
+   (reduce (fn [a b] (if (> a b) a b)) a-seq)
+  ])
 
 (defn insert [sorted-seq n]
   [:-])
