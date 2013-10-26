637c191025a62dfd2a2f11f7a1408d27b5b25f5e
zHarrowed <eetu_mattila@hotmail.com>
2013-10-27 01:23:33 +0300

    This should be enough for total of 100 excercises I hope, I'm done. Gonna try 4th credit next year

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,22 +1,36 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat () a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce str (interpose " " a-seq))))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (let [f (fn [aseq string] (conj (conj aseq string) x))]
+    (cond
+      (empty? a-seq) '()
+      (empty? (rest a-seq)) a-seq
+      :else (reverse (rest (reverse (reduce f [] a-seq)))))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [count x] (inc count))]
+    (if (empty? a-seq)
+      0
+      (reduce counter 0 a-seq))))
 
 (defn my-reverse [a-seq]
   [:-])
 
 (defn min-max-element [a-seq]
-  [:-])
+  (let [f (fn [a-vec x] (let [[a b] a-vec]
+                          (assoc
+                            (assoc a-vec 0 (min a x)) 1 (max b x))))]
+    (if (empty? a-seq)
+      []
+      (reduce f (vector (first a-seq) (first a-seq)) a-seq))))
 
 (defn insert [sorted-seq n]
   [:-])
