f0a7101872c73b53ca7c82897b3c304dc478ab3a
JRG <donjorgo@gmail.com>
2013-10-27 20:27:47 +0200

    välicommit

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,19 +1,23 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce (fn [s1 s2] (str s1 " " s2)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    []
+    (reduce (fn [e1 e2] (conj e1 x e2)) [(first a-seq)] (rest a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [e-count e] (inc e-count)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [rev e] (conj rev e)) '() a-seq))
 
 (defn min-max-element [a-seq]
   [:-])
