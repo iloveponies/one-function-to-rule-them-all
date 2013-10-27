ce896ccbf11e937eb368a5db68b96c521751f5c2
Aleksi Paavola <ahpaavola@gmail.com>
2013-10-27 14:41:59 +0200

    one function to rule them all, all test except encore passed

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,43 +1,80 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq))
+
 
 (defn str-cat [a-seq]
-  :-)
+  (str (clojure.string/join " " a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (or (empty? a-seq) (= 1 (count a-seq)))
+  a-seq
+  (flatten (reduce (fn [acc next]
+            (concat [acc] [x] [next])) a-seq))))
+
 
 (defn my-count [a-seq]
-  :-)
+  (if (empty? a-seq)
+    0
+    (reduce (fn [c _]
+              (inc c)) 0 a-seq)
+    ))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce conj () a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (vector (reduce min a-seq)
+          (reduce max a-seq)))
 
 (defn insert [sorted-seq n]
-  [:-])
+   (concat (filter #(< % n) sorted-seq)
+           [n]
+           (filter #(> % n) sorted-seq)))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (reduce (fn [aseq k]
+              (insert aseq k)) '() a-seq)))
 
 (defn parity [a-seq]
-  [:-])
+ (set (reverse (reduce (fn [aseq k]
+                (if (some #(= % k) aseq)
+                 (remove #(= % k) aseq)
+                 (conj aseq k))) () a-seq))))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& args]
+  (reduce (fn [x _]
+            (inc x)) 0 args))
 
-(defn my-* [x]
-  :-)
+(defn my-*
+  ([] 1)
+  ([x] x)
+  ([x y] (* x y))
+  ([x y & args] (reduce * (* x y) args)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([] (fn [z]
+        (= z z)))
+  ([p] (fn [z]
+         (p z)))
+  ([pred1 pred2] (fn [z]
+           (and
+            (pred1 z)
+            (pred2 z))))
+  ([x y & preds] (fn [z]
+                   (reduce
+                    #(and %1 (%2 z))
+                    (and
+                     (x z)
+                     (y z))
+                    preds))))
 
 (defn my-map [f a-seq]
   [:-])
