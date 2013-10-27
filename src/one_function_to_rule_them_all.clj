c63f4e373344fb3e74fece906ee5c6c341ed89dd
unknown <miika.kalske@luukku.com>
2013-10-27 21:57:48 +0200

    not fully done

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -6,40 +6,64 @@
 (defn str-cat [a-seq]
   (if (empty? a-seq)
     ""
-    (reduce str (+ a-seq " "))))
+    (reduce (fn [a b] (str a " " b)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (reduce conj a-seq x)))
 
 (defn my-count [a-seq]
-  :-)
+  (let [my-counter (fn [co s]
+                     (inc co))]
+  (reduce my-counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (let [rev (fn [old rev-seq]
+              (cons (last old) rev-seq))]
+    (reduce rev a-seq)))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [vec se] [(min (get vec 0) se) (max (get vec 1) se)]) [(first a-seq) (first a-seq)] a-seq))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (let [indx (fn [idx sort-seq n]
+               (cond
+                (empty? sort-seq)
+                 idx
+                (> (first sort-seq) n)
+                 idx
+                :else (recur (inc idx) (rest sort-seq) n)))]
+    (concat (take (indx 0 sorted-seq n) sorted-seq) (conj (drop (indx 0 sorted-seq n) sorted-seq)n))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert [] a-seq))
 
 (defn parity [a-seq]
-  [:-])
+  (let [helper (fn [parities elem]
+                 (if (contains? parities elem)
+                   (disj parities elem)
+                   (conj parities elem)))]
+    (reduce helper #{} a-seq)))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (unchecked-negate-int x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& params] (count params))
 
-(defn my-* [x]
-  :-)
+(defn my-*
+  ([] 1)
+  ([x] 1)
+  ([x y] (* x y))
+  ([x y & more] (reduce * (* x y) more)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([] (fn [p] true))
+  ([p] p)
+  ([p1 p2] (fn [p] (and (p1 p) (p2 p))))
+  ([p1 p2 & more] (reduce pred-and (pred-and p1 p2) more)))
 
 (defn my-map [f a-seq]
   [:-])
+
