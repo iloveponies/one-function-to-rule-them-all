02896b682bd39558a7126f2bdfe88534463140b7
vuojola <lauri.vuojola@helsinki.fi>
2013-10-27 21:25:53 +0200

    eighth set partially done

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,22 +1,26 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce (fn [a b] (str a " " b)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '()
+    (reduce (fn [a b] (conj a x b)) (vector (first a-seq)) (rest a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [i elem] (inc i)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [a b] (cons b a)) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  [(reduce min a-seq) (reduce max a-seq)])
 
 (defn insert [sorted-seq n]
   [:-])
