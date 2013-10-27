99eaa8d4916c0c67d769ba789d13247a385624bd
Pia Pakarinen <pia.pakarinen@helsinki.fi>
2013-10-27 21:03:59 +0200

    get dat 3 op?

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,25 +1,30 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat () a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (let [funk (fn [x y] (str x " " y))]
+   (if (empty? a-seq)
+     ""
+     (reduce funk a-seq))))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (let [fudge (fn [z y] (conj x y))]
+  (reduce fudge (first a-seq) (rest a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [x e] (inc x))]
+    (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+   (reduce conj () a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (vector (reduce min a-seq) (reduce max a-seq)))
 
 (defn insert [sorted-seq n]
-  [:-])
+  nil)
 
 (defn insertion-sort [a-seq]
   [:-])
@@ -27,17 +32,29 @@
 (defn parity [a-seq]
   [:-])
 
-(defn minus [x]
-  :-)
+(defn minus ([x] (- 0 x))
+             ([x y] (- x y)))
 
 (defn count-params [x]
-  :-)
+  (reduce inc 0 x))
 
-(defn my-* [x]
-  :-)
+(defn my-* ([] 1)
+            ([x] x)
+            ([x y] (* x y))
+            ([x y & more] (reduce * (* x y) more))
+  )
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+               ([] (fn [x] true))
+               ([p] p)
+               ([p b] (fn [x] (if (and (p x) (b x)) true false)))
+               ([p b & more]
+                (reduce pred-and
+                       (fn [x] (if (and (p x) (b x))
+                                   true
+                                   false))
+                        more)))
+
 
 (defn my-map [f a-seq]
   [:-])
