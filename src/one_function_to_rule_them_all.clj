e8ec930e1d6bf5e5bc76f3c1d36b329e49d7234c
Eero Sääksvuori <esaaksvu@cs.helsinki.fi>
2013-10-27 03:36:46 +0300

    one function to... 6/16

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,22 +1,26 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (concat (list) (reduce concat a-seq)))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+      (reduce str (interpose " " a-seq))))
 
 (defn my-interpose [x a-seq]
   [:-])
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [count e]
+                    (inc count))]
+    (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
   [:-])
 
 (defn min-max-element [a-seq]
-  [:-])
+  [(reduce min a-seq) (reduce max a-seq)])
 
 (defn insert [sorted-seq n]
   [:-])
@@ -27,11 +31,12 @@
 (defn parity [a-seq]
   [:-])
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params
+  ([& params] (count params)))
 
 (defn my-* [x]
   :-)
