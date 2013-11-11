b3e4dc5c12acf5474cf2f119aa73cb96b2bfb5c9
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:51:15 +0200

    Tests as a separate project

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject one-function-to-rule-them-all "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje-grader "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.1.1"]]}})
+                 [iloveponies.tests/one-function-to-rule-them-all "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
