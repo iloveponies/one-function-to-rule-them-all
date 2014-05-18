28cb113d6c8313ebbb5450470272daa1f0d27247
kaz-yos <kazky@mac.com>
2014-05-18 10:13:30 -0400

    q1

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,7 +1,26 @@
-(ns one-function-to-rule-them-all)
+(ns one-function-to-rule-them-all
+  (:require [clojure.test :as ctest]))
 
+
+;; Exercise 1
+;; Write the function (concat-elements a-seq) that takes a sequence of sequences and concatenates them together with concat.
+;; Don’t use apply to implement this function.
+;;
+;; sig: seq of seqs -> seq
+;; purpose flatten a nested structure
+;; stab:
+;; (defn concat-elements [a-seq]
+;;   :-)
+;;
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
+;;
+(ctest/is (= (concat-elements [])            ()))
+;; these fail but it is working??
+(ctest/is (= (concat-elements [[1 2]])       (1 2)))
+(ctest/is (= (concat-elements [[1 2] [3 4]]) (1 2 3 4)))
+
+
 
 (defn str-cat [a-seq]
   :-)
