b1774086ab9e469f29a05c519fa413a1f32c5dae
kaz-yos <kazky@mac.com>
2014-05-18 22:02:11 -0400

    insertion sort

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -125,27 +125,57 @@
 (ctest/is (= (min-max-element [1])           [1 1]))
 
 
-Exercise 7
-Write the function (insert sorted-seq n) that adds the number n into a sorted sequence of number. The ordering of the sequence must be preserved.
-You don’t need to use reduce for this, and you probably don’t want to.
+;; Exercise 7
+;; Write the function (insert sorted-seq n) that adds the number n into a sorted sequence of number. The ordering of the sequence must be preserved.
+;; You don’t need to use reduce for this, and you probably don’t want to.
+;;
+;; signature: seq, number -> seq
+;; purpose: insert number to a sequence
+;; stub
+;; (defn insert [sorted-seq n]
+;;   [nil nil nil])
+;;
 (defn insert [sorted-seq n]
-  [:-])
+  (loop [passed    []
+         remaining sorted-seq]
+    (cond
+     ;; Exiting at the end of seq
+     (empty? remaining)                 (conj passed n)
+     ;; Exisiting in the middle of seq
+     (< n (first remaining))            (concat (conj passed n) remaining)
+     ;; Otherwise recur
+     :else                              (recur (conj passed (first remaining)) (rest remaining)))))
 ;;
 (ctest/is (= (insert [] 2)       '(2)))
 (ctest/is (= (insert [1 3 4] 2)  '(1 2 3 4)))
 (ctest/is (= (insert [1] 2)      '(1 2)))
 
-Now implement (insertion-sort a-seq) using reduce and the function insert.
+;; Now implement (insertion-sort a-seq) using reduce and the function insert.
+;;
+;; sig: seq -> seq
+;; Sort by insertion
+;; stub
+;; (defn insertion-sort [a-seq]
+;;   [nil nil nil])
+;;
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert [] a-seq))
+;;
 (ctest/is (= (insertion-sort [2 5 3 1])  '(1 2 3 5)))
 (ctest/is (= (insertion-sort [1 2])      '(1 2)))
 
 
-Exercise 8
-Write the fuction (parity a-seq) that picks into a set those elements of a-seq that occur odd number of time.
+;; Exercise 8
+;; Write the fuction (parity a-seq) that picks into a set those elements of a-seq that occur odd number of time.
+;;
+;; 
+;; stub:
 (defn parity [a-seq]
   [:-])
+;;
+(defn parity [a-seq]
+  [:-])
+;;
 (ctest/is (= (parity [:a :b :c])     #{:a :b :c}))
 (ctest/is (= (parity [:a :a :b :b])  #{}))
 (ctest/is (= (parity [1 2 3 1])      #{2 3}))
