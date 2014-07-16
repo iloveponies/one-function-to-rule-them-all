eb58ecf5f61bb655fc737be722a2a2fe1c81e92a
bernhard <bernhard.maeder@gmail.com>
2014-07-16 17:32:40 +0200

    Working on it.

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,13 +1,21 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat () a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (let [f (fn
+            ([]        ())
+            ([e]       (list e))
+            ([acc e]   (if (not (empty? acc))
+                         (concat acc (list x e))
+                         (list e))))]
+    (reduce f () a-seq)))
 
 (defn my-count [a-seq]
   :-)
@@ -41,3 +49,4 @@
 
 (defn my-map [f a-seq]
   [:-])
+
