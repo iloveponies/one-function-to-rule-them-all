f0691df15b6613fe342513d587db4135a75d181f
Sean Darcy <darcys22@gmail.com>
2014-07-25 04:46:39 -0700

    this is more fun

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,28 +1,53 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) ""
+  (reduce
+   (fn [x y]
+     (str x " " y))
+   a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (reduce
+   (fn [y ys]
+     (into [] (y x ys)))
+   a-seq))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce
+   (fn [x y]
+     (inc x))
+   0
+   a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce conj '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce
+    (fn [x y]
+     (let [[min max] x]
+       (cond
+        (< y min) [y max]
+        (> y max) [min y]
+        :else [min max])))
+    [(first a-seq) (first a-seq)]
+    (rest a-seq)))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (cond
+   (empty? sorted-seq) (list n)
+   (< n (first sorted-seq)) (cons n sorted-seq)
+   :else (cons (first sorted-seq) (insert (rest sorted-seq) n))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce
+   insert
+   '()
+   a-seq))
 
 (defn parity [a-seq]
   [:-])
