6f44ee03f97521e7271d5f4ea933eef687bdc811
simon2e <simon@tuohey.net>
2014-08-25 23:33:54 +1000

    on mymap

--- repl/scratch.clj
+++ repl/scratch.clj
@@ -2,9 +2,52 @@
 
 (my-count [])      ;=> 0
 (my-count [1 2 3]) ;=>
-(reduce (fn ([a b] str "(f:" a ") s: (" b ")" ) [1 2 3])
+(my-count [1 2 3 8])     ;=> 1
 
-(my-count [1])     ;=> 1
+(my-reverse [1 2 3]) ;=> (3 2 1)
+(my-reverse [1 2])   ;=> (2 1)
+(my-reverse [])      ;=> ()
 
-(reduce (fn [a b] (inc a)) 0 [])
+(min-max-element [2 7 3 15 4]) ;=> [2 15]
+(min-max-element [1 2 3 4])    ;=> [1 4]
+(min-max-element [1])          ;=> [1 1]
+(min-max-element [])          ;=> [1 1]
 
+(insert [] 2)      ;=> (2)
+(insert [1 3 4] 2) ;=> (1 2 3 4)
+(insert [1] 2)     ;=> (1 2)
+
+(insertion-sort [2 5 3 1]) ;=> (1 2 3 5)
+(insertion-sort [1 2])     ;=> (1 2)
+
+(parity [:a :b :c])    ;=> #{:a :b :c}
+(parity [:a :a :b :b]) ;=> #{}
+(parity [1 2 3 1])     ;=> #{2 3}
+
+(minus 2)   ;=> -2
+(minus 4 3) ;=> 1
+
+(count-params)            ;=> 0
+(count-params :a)         ;=> 1
+(count-params :a 1 :b :c) ;=> 4
+
+(my-*)           ;=> 1
+(my-* 4 3)       ;=> 12
+(my-* 1 2 3 4 5) ;=> 120
+
+(filter (pred-and) [1 0 -2])                    ;=> (1 0 -2)
+(filter (pred-and even?) [1 0 -2])                    ;=> (1 0 -2)
+(filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3]) ;=> (1 7)
+(filter (pred-and number? integer? pos? even?)
+        [1 0 -2 :a 7 "a" 2])                    ;=> (0 2)
+
+(my-map inc [1 2 3 4])                  ;=> (2 3 4 5)
+(my-map + [1 1 1] [1 1 1] [1 1 1])      ;=> (3 3 3)
+(my-map vector [1 2 3] [1 2 3] [1 2 3]) ;=> ((1 1 1) (2 2 2) (3 3 3))
+
+(def fred (fn [& colls]
+            (loop [cls colls
+                   acc '()]
+              (if (empty? cls)
+                acc
+                (recur (next cls) (cons (ffirst cls) acc) )))))
--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -17,34 +17,69 @@
      (seq (reduce #(conj %1 x %2) (vector (first coll)) (next coll))))))
 
 (defn my-count [a-seq]
-    (reduce (fn (inc %1)) 0 a-seq))
+    (reduce (fn [acc item] (inc acc)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce #(cons %2 %1) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce
+   (fn [[amin amax] item]    ; start with [min max] acc
+     (let
+       [newmin (if (< item amin) item amin)
+        newmax (if (> item amax) item amax)]
+       (vector newmin newmax)))
+   [(first a-seq) (first a-seq)]
+   (next a-seq)))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (cond
+   (empty? sorted-seq)
+   (list n)
+   (< n (first sorted-seq))
+   (cons n sorted-seq)
+   :else
+   (cons (first sorted-seq) (insert (next sorted-seq) n))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert '() a-seq))
 
 (defn parity [a-seq]
-  [:-])
+  (let [toggle (fn [aset item]
+                 (if-not (aset item) (conj aset item) (disj aset item)))]
+    (reduce toggle #{} a-seq)))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- 0 x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& params]
+  (count params))
 
-(defn my-* [x]
-  :-)
+(defn my-*
+  ([] 1)
+  ([x] x)
+  ([x y] (* x y))
+  ([x y & more]
+   (reduce * (* x y) more)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([] (fn [x] true))
+  ([& params]
+   (fn [x]
+     (loop [preds params]
+       (cond
+        (empty? preds)
+        true
+        (not ((first preds) x))
+        false
+        :else
+        (recur (next preds)))))))
 
-(defn my-map [f a-seq]
-  [:-])
+(defn my-map
+  ([f a-seq]
+   (if (empty? a-seq)
+   '()
+   (cons (f (first a-seq)) (my-map f (next a-seq))))))
+;  ([f a-seq & more-seq])
+;  )
