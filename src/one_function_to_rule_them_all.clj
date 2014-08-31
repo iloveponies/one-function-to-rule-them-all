96e8f4b149a7ca12022e4139e43a0430fc8930cc
Chris Howe-Jones <chris.howejones@gmail.com>
2014-08-31 10:42:58 +0100

    Implemented up to my-interpose

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,13 +1,16 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (reduce #(conj %1 %2 x) [] a-seq))
+
 
 (defn my-count [a-seq]
   :-)
