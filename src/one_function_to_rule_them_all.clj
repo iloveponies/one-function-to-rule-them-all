11dbbb1fa73677dcd974191982e7de1750dd3228
Fabio Utzig <utzig@utzig.org>
2014-10-07 19:25:30 -0300

    First 4 exercises

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,16 +1,23 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (let [concat-with-spaces (fn [s1 s2] (str s1 " " s2))]
+    (if (empty? a-seq)
+      ""
+      (reduce concat-with-spaces a-seq))))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (let [concat-with-interpose (fn [s1 s2]
+                                (conj s1 x s2))]
+    (rest (reduce concat-with-interpose [] a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [count-elements (fn [counter elem]
+                         (inc counter))]
+    (reduce count-elements 0 a-seq)))
 
 (defn my-reverse [a-seq]
   [:-])
