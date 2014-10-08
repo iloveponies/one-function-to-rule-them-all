a9a3254ed0adebd9de5d2795c5279bca76f41e33
Fabio Utzig <utzig@utzig.org>
2014-10-08 11:37:05 -0300

    Finish exercises minus my-map

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -20,31 +20,52 @@
     (reduce count-elements 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (let [reverse-elements (fn [e1 e2]
+                           (conj e1 e2))]
+    (reduce reverse-elements '() a-seq)))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    []
+    (let [min-val (reduce min a-seq)]
+      (let [max-val (reduce max a-seq)]
+        [min-val max-val]))))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (if
+    (empty? sorted-seq) [n]
+    (let [x (first sorted-seq)]
+      (if (< x n)
+        (cons x (insert (rest sorted-seq) n))
+        (cons n (cons x (rest sorted-seq)))))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert [] a-seq))
 
 (defn parity [a-seq]
-  [:-])
+  (let [toggle (fn [t-set elem]
+    (if (contains? t-set elem)
+      (disj t-set elem)
+      (conj t-set elem)))]
+    (reduce toggle #{} a-seq)))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x]   (* x -1))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& x]
+  (count x))
 
-(defn my-* [x]
-  :-)
+; since reducing * already provides the expected functionality there's
+; no need to write conditional code based on number of arguments
+(defn my-* [& args]
+  (reduce * args))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and [& args]
+  (fn [x]
+    (let [nargs (count args)]
+      (if (zero? nargs) true
+      (reduce (fn [y f] (and y (f x))) '() args)))))
 
 (defn my-map [f a-seq]
   [:-])
