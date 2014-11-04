9f45e80fa9d353900d311f82a60e62aabac566d8
s0rbus <soorbus@gmail.com>
2014-11-04 20:51:58 +0000

    finished ex 5

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,19 +1,25 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+  (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (reduce #(if (empty? %1)
+       (conj %1 %2)
+       (conj %1 x %2)) []  a-seq))
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [count e]
+                    (inc count))]
+    (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce conj '() a-seq))
 
 (defn min-max-element [a-seq]
   [:-])
