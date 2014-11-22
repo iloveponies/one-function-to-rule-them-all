5767c8de767d038f4c29b95eceac7a5c4ab261a9
Patrick Martin <prof.pmartin10@gmail.com>
2014-11-22 13:45:58 -0500

    Rocked insertion sort.

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -31,13 +31,23 @@
     (reduce rev-fn `() a-seq)))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (let [min-max-fn (fn [min-max-vec e]
+                     (if (empty? min-max-vec)
+                       [e e]
+                       (cond
+                         (< e (first min-max-vec)) (assoc min-max-vec 0 e)
+                         (> e (last min-max-vec)) (assoc min-max-vec 1 e)
+                         :else min-max-vec)))]
+    (reduce min-max-fn [] a-seq)))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (let [less-seq (filter (fn [e] (< e n)) sorted-seq)
+        more-seq (filter (fn [e] (> e n)) sorted-seq)]
+    (concat less-seq [n] more-seq)))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce (fn [sorted-seq n]
+            (insert sorted-seq n)) [] a-seq))
 
 (defn parity [a-seq]
   [:-])
