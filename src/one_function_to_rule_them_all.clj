3a4d58717b95a4364e0ebd317a9adaeb4d32fbfd
mrLite <aku.peltonen@intelle.fi>
2014-12-04 23:28:21 +0200

    exercises up to 'my-map'

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,43 +1,59 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) "" (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    (sequence a-seq)
+    (sequence (reduce #(conj %1 x %2) [(first a-seq)] (rest a-seq)))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [len _] (inc len)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [xs x] (conj xs x)) () a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (let [min-max (fn [[mn mx] x]
+                  [(min mn x) (max mx x)])
+        fst (first a-seq)
+        value (if (empty? a-seq) [] [fst fst])]
+    (reduce min-max value (rest a-seq))))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (let [[s l] (split-with #(<= % n) sorted-seq)]
+    (concat s (seq [n]) l)))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert () a-seq))
 
 (defn parity [a-seq]
-  [:-])
+  (let [odd-freqs (filter #(odd? (val %)) (frequencies a-seq))]
+    (set (keys odd-freqs))))
 
-(defn minus [x]
+(defn minus
+  ([x] (minus 0 x))
+  ([x y] (- x y)))
+
+(defn count-params [& args]
+  (count args))
+
+(defn my-*
+  ([] 1)
+  ([x] x)
+  ([x y] (* x y))
+  ([x y & xs] (reduce my-* (my-* x y) xs)))
+
+(defn pred-and
+  ([] (fn [_] true))
+  ([p] p)
+  ([p1 p2] (fn [x] (and (p1 x) (p2 x))))
+  ([p1 p2 & preds]
+   (reduce #(pred-and %1 %2) (pred-and p1 p2) preds)))
+
+(defn my-map [f c & cs]
   :-)
-
-(defn count-params [x]
-  :-)
-
-(defn my-* [x]
-  :-)
-
-(defn pred-and [x]
-  (fn [x] :-))
-
-(defn my-map [f a-seq]
-  [:-])
