0c5725f88b3e2dcc77f79259af9addedb965ffbd
Julian Jelfs <julian.jelfs@gmail.com>
2015-01-01 22:58:29 +0000

    first couple

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,10 +1,11 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) ""
+    (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
   [:-])
