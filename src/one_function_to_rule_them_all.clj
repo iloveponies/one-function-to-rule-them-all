0398b1e84788d08e4b6eab7c00e19a883cd57ed5
Julian Jelfs <julian.jelfs@gmail.com>
2015-01-03 22:13:00 +0000

    count and reverse

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -15,10 +15,16 @@
     (reduce #(conj %1 x %2) [(first a-seq)] (rest a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [count e]
+                  (inc count))]
+    (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) a-seq
+    (= 1 (count a-seq)) a-seq
+    :else
+    (reduce #(conj %1 %2) (list (first a-seq)) (rest a-seq))))
 
 (defn min-max-element [a-seq]
   [:-])
