9065ffa747f637c51d67d4aabb4dbb22a56c4cb9
Ilari Oras <happo1472583@gmail.com>
2015-02-01 02:29:41 +0200

    ääh, nukkumaan

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -9,19 +9,18 @@
                (reduce (fn [acc, x] (str acc " " x)) (rest a-seq))])))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq) '()
+    (drop-last (reduce (fn [acc j] (conj (conj acc j) x)) [] a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [acc j] (inc acc)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [acc i] (conj acc (conj acc i))) [] a-seq))
 
-(defn min-max-element [a-seq]
-  [:-])
+(defn min-max-element [a-seq] [:-])
 
-(defn insert [sorted-seq n]
-  [:-])
+(defn insert [sorted-seq n] [:-])
 
 (defn insertion-sort [a-seq]
   [:-])
