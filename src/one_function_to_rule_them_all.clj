7d6d6eba40d84b5a5adaeb52a9fd5e5a4e11f89c
Ilari Oras <happo1472583@gmail.com>
2015-02-02 14:36:27 +0200

    ääh

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -21,26 +21,41 @@
 (defn min-max-element [a-seq]
   (reduce (fn [[mi ma] i] [(min mi i) (max ma i)]) [9999 -1] a-seq))
 
-(defn insert [sorted-seq n] [:-])
+(defn insert [sorted-seq n]
+  (concat (take-while #(<= % n) sorted-seq) [n] (drop-while #(<= % n) sorted-seq)))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce (fn [acc n] (insert acc n)) [] a-seq))
+
+(defn toggle [seq-1 eleme]
+    (if (contains? seq-1 eleme)
+          (disj seq-1 eleme)
+          (conj seq-1 eleme)))
 
 (defn parity [a-seq]
-  [:-])
+  (reduce toggle #{} a-seq))
 
 (defn minus
   ([x] (- 0 x))
   ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params
+  ([] 0)
+  ([x] 1)
+  ([x y] 2)
+  ([x y & more] (reduce (fn [acc x] (inc acc)) 2 more)))
 
-(defn my-* [x]
-  :-)
+(defn my-*
+  ([] 1)
+  ([x] x)
+  ;([x y] (* x y))
+  ([x y & more] (reduce (fn [acc i] (* acc i)) (* x y) more)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([] true)
+  ([x] x)
+  ([x y] (and x y))
+  ([x y & more] (reduce (fn [acc i] (and acc i)) (and x y) more)))
 
 (defn my-map [f a-seq]
   [:--])
