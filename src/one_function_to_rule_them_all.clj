1dcff4cb90917b2506851d7bb1f9f04b532bda50
Kaltsoon <Kalle.ilves@helsinki.fi>
2015-02-12 10:32:16 +0200

    getting started with one function to rule them all

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,22 +1,28 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce (fn [acc elem] (str acc " " elem)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    []
+    (reduce (fn [acc elem] (conj acc x elem)) [(first a-seq)] (rest a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [s _](inc s)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce #(cons %2 %) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (if (empty? a-seq) []
+  (let [f (first a-seq)]
+    (reduce #(identity [(min (% 0) %2) (max (% 1) %2)]) [f f] (rest a-seq)))))
 
 (defn insert [sorted-seq n]
   [:-])
