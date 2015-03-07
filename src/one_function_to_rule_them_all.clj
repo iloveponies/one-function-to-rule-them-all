18a431bc304bcd56f507bd39f4462924e90b3285
jykke <jyri.lehva@saunalahti.fi>
2015-03-07 10:45:37 +0200

    exercise 3 my-interpose

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -9,7 +9,9 @@
     (reduce (fn [x y] (str x " " y)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '()
+  (concat (reduce (fn [a b] (conj (conj a x) b)) [(first a-seq)] (rest a-seq)))))
 
 (defn my-count [a-seq]
   :-)
