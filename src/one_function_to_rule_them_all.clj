74ea5be9f828022eb9687c9efe4fa5ebf985a6c0
jarcane <jarcane@users.noreply.github.com>
2015-03-28 10:22:12 +0200

    Old solution for intersperse

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,13 +1,19 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) []
+   (and (not (empty? a-seq))
+        (empty? (rest a-seq))) a-seq
+   :else (flatten (reduce #(vec [%1 x %2]) a-seq))))
 
 (defn my-count [a-seq]
   :-)
