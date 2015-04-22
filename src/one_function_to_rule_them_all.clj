99594fbfaed5eb6ec8d2b5aacab18b6581bafeb3
Pietro F. Menna <pietromenna@yahoo.com>
2015-04-22 11:57:55 -0400

    Exercise 5

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,19 +1,28 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
-
+ (if (empty? a-seq)
+   ""
+   (reduce (fn [a b] (str a " " b)) a-seq )))
+ 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '()
+    (reverse (drop 1 (reduce (fn [a b] (conj a b x) ) '() a-seq))))
+  )
 
 (defn my-count [a-seq]
-  :-)
+  (if (empty? a-seq)
+    0
+    (reduce (fn [a b] (inc a)) 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '()
+    (reduce (fn [a b] (conj a b)) '() a-seq)))
 
 (defn min-max-element [a-seq]
   [:-])
