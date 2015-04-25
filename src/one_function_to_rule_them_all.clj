4758a02b3ec563078e2ce8907baea6d0913336cf
satukark <Satu@82-181-230-236.bb.dnainternet.fi>
2015-04-25 20:11:59 +0300

    1-5 One Function
    One Function to rule them all

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,19 +1,22 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) ""
+  (reduce (fn [x z] (str x " " z)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (rest (reduce (fn [y z] (conj y z x)) () (reverse a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [add (fn [x _] (inc x))]
+  (reduce add 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (let [rev (fn [x y] (cons y x))]
+  (reduce rev () a-seq)))
 
 (defn min-max-element [a-seq]
   [:-])
