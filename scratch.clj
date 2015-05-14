365fb28e8ed95cd9626cecb413a1bddd03c8e4ab
Flavio Barisi <flvb86@gmail.com>
2015-05-14 17:59:54 +0200

    Fix

--- scratch.clj
+++ scratch.clj
@@ -14,3 +14,44 @@
 (my-interpose "," ["I" "me" "myself"]) ;=> ("I" "," "me" "," "myself")
 (my-interpose :a [1])                  ;=> (1)
 (my-interpose :a [])                   ;=> ()
+(my-interpose :b [[1] :a [2] 3])               ;=> (1 0 2 0 3)
+
+(my-count [])      ;=> 0
+(my-count [1 2 3]) ;=> 3
+(my-count [1])     ;=> 1
+
+(my-reverse [1 2 3]) ;=> (3 2 1)
+(my-reverse [1 2])   ;=> (2 1)
+(my-reverse [])      ;=> ()
+
+(min-max-element [2 7 3 15 4]) ;=> [2 15]
+(min-max-element [1 2 3 4])    ;=> [1 4]
+(min-max-element [1])          ;=> [1 1]
+(min-max-element [])          ;=> []
+
+(insert [] 2)      ;=> (2)
+(insert [1 3 4] 2) ;=> (1 2 3 4)
+(insert [1] 2)     ;=> (1 2)
+
+(insertion-sort [2 5 3 1]) ;=> (1 2 3 5)
+(insertion-sort [1 2])     ;=> (1 2)
+
+(parity [:a :b :c])    ;=> #{:a :b :c}
+(parity [:a :a :b :b]) ;=> #{}
+(parity [1 2 3 1])     ;=> #{2 3}
+
+(minus 2)   ;=> -2
+(minus 4 3) ;=> 1
+
+(count-params)            ;=> 0
+(count-params :a)         ;=> 1
+(count-params :a 1 :b :c) ;=> 4
+
+(my-*)           ;=> 1
+(my-* 4 3)       ;=> 12
+(my-* 1 2 3 4 5) ;=> 120
+
+(filter (pred-and) [1 0 -2])                    ;=> (1 0 -2)
+(filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3]) ;=> (1 7)
+(filter (pred-and number? integer? pos? even?)
+        [1 0 -2 :a 7 "a" 2])                    ;=> (0 2)
--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -9,38 +9,42 @@
 
 (defn my-interpose [x a-seq]
   (cond (empty? a-seq) '()
-        (= 1 (count a-seq)) (seq a-seq)
-        :else (seq (reduce #(if (coll? %1) (conj %1 x %2) [%1 x %2]) a-seq))))
+        :else (seq (reduce #(conj %1 x %2) [(first a-seq)] (rest a-seq)))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [x1 x2] (+ x1 1)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [x1 x2] (conj x1 x2)) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [x1 x2] (if (empty? x1)
+                        [x2 x2]
+                        [(min (nth x1 0) x2) (max (nth x1 1) x2)]))
+          [] a-seq))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (let [ [x1 x2] (split-with #(< % n) sorted-seq)]
+    (concat x1 (cons n x2))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert '() a-seq))
 
 (defn parity [a-seq]
-  [:-])
+  (reduce #(if (contains? %1 %2) (disj %1 %2) (conj %1 %2)) #{} a-seq))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& more]
+  (count more))
 
-(defn my-* [x]
-  :-)
+(defn my-* [& more]
+  (reduce * 1 more))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and [& more]
+  (fn [x] (reduce #(and %1 (%2 x)) true more)))
 
 (defn my-map [f a-seq]
   [:-])
