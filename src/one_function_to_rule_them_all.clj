4270a42e06bcd99958ba7d646f9e1042e107fd8b
evgeny <evgeny@air.local>
2015-05-20 12:16:14 +0300

    1-4

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,16 +1,22 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) 
+  	""
+    (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq) 
+  	[]
+    (reduce #(conj %1 x %2) [(first a-seq)] (rest a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (if (empty? a-seq)
+    0
+    (reduce (fn [c _] (inc c)) 0 a-seq)))
 
 (defn my-reverse [a-seq]
   [:-])
