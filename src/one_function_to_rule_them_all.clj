5baf6ddc1108fe07b7bd6d98edc4081e853c12f9
a544jh <axel.wikstrom@gmail.com>
2015-08-11 22:33:30 +0300

    more done

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -9,18 +9,19 @@
     (reduce (fn [a b] (str a " " b)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  (if (empty? a-seq)
+  (if (<= (count a-seq) 2)
     a-seq
-    (reduce (fn [a b] (conj (conj a x) b)) a-seq)))
+    (let [acc [(first a-seq) x (second a-seq)]]
+    (reduce (fn [a b] (conj (conj a x) b)) acc (drop 2 a-seq)))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [a b] (inc a)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [a b] (cons b a)) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [a b] [(min (first a) b) (max (second a) b)]) [(first a-seq) (first a-seq)] a-seq))
 
 (defn insert [sorted-seq n]
   [:-])
