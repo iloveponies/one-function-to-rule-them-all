f0160478e984e9550a35e51735863eff285e8a44
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-07-27 06:05:19 +0300

    encore tehty

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -62,5 +62,38 @@
                   (a x)
                   (apply (apply pred-and more) (list x))))))
 
-(defn my-map [f a-seq]
-  [:-])
+
+
+(defn any-exhausted
+  ([] false)
+  ([a] (empty? a))
+  ([a & more] (or
+                (empty? a)
+                (apply any-exhausted more))))
+
+(defn map-firsts
+  ([] `())
+  ([a] (list (first a)))
+  ([a & more] (concat (list (first a)) (apply map-firsts more))))
+
+(defn without-firsts
+  ([] `())
+  ([a] (list (rest a)))
+  ([a & more] (concat (list (rest a)) (apply without-firsts more))))
+
+(defn my-map-helper [output f & seqs]
+  (if (apply any-exhausted seqs)
+    output
+    (recur
+      (conj output (apply f (apply map-firsts seqs)))
+      f
+      (apply without-firsts seqs))))
+
+(defn my-map [f & seqs]
+  (seq (apply my-map-helper [] f seqs)))
+
+(my-map vector [1 2 3] [1 2 3] [1 2 3])
+
+(my-map inc [1 2 3])
+
+(my-map + [1 1 1] [1 1 1] [1 1 100])
