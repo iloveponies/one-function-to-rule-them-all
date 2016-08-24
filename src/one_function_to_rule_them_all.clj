cf25ea3c111203164c962fe466c5098adaf31288
Mikko <mikko.o.niemi@gmail.com>
2016-08-24 12:01:13 +0300

    Tehtävät 1 - 4

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,16 +1,31 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce
+     (fn [sana1 sana2] (str sana1 " " sana2)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (loop [sekvenssi a-seq
+         eka (first sekvenssi)
+         loput (rest sekvenssi)
+         tulos []]
+    (cond
+     (empty? sekvenssi) (sequence tulos)
+     (empty? loput) (seq (conj tulos eka))
+     :else (recur loput
+                  (first loput)
+                  (rest loput)
+                  (conj (conj tulos eka) x)))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [apuri (fn [summa elem]
+                  (inc summa))]
+    (reduce apuri 0 a-seq)))
 
 (defn my-reverse [a-seq]
   [:-])
