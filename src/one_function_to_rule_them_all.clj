2f1a6bb10dda69c90be6c2e243c8216aa50f2d59
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-10-24 20:37:21 +0300

    Solve concats

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,10 +1,11 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) ""
+   (reduce (fn [a b] (str a " " b)) a-seq)))
 
 (defn my-interpose [x a-seq]
   [:-])
