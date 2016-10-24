b0a6176a3b0c5aad2b50a32939dbe3a5a1de04aa
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-10-24 21:26:49 +0300

    Solve pred-and

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -54,8 +54,11 @@
   ([x y] (* x y))
   ([x y & more] (reduce my-* (my-* x y) more)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([]      (fn [x] true))
+  ([p]     (fn [x] (p x)))
+  ([p1 p2] (fn [x] (and (p1 x) (p2 x))))
+  ([p1 p2 & more] (reduce pred-and (pred-and p1 p2) more)))
 
 (defn my-map [f a-seq]
   [:-])
