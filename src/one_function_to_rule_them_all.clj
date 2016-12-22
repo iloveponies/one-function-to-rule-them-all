ded8a65072c7519e87a485587c50fe1616c7a7e4
thakuli <tero.hakuli@nokia.com>
2016-12-22 09:01:25 +0200

    tests

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -15,19 +15,43 @@
   (rest (reduce #(conj %1 x %2) [] a-seq)))
 
 (defn my-count [a-seq]
-  :-)
+  (let [ counter (fn [count e]
+                   (inc count)) ]
+    (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (let [ helper (fn [reversed e]
+                  (cons e reversed)) ]
+    (reduce helper [] a-seq)))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (let [ helper (fn [min-max e]
+                  (if (empty? min-max) 
+                    [e e]
+                    (do
+                      (cond 
+                        (< e (first min-max)) [e (last min-max)]
+                        (> e (last min-max)) [(first min-max) e]
+                        :else min-max)))) ]
+    (reduce helper [] a-seq)))
+                    
 
 (defn insert [sorted-seq n]
-  [:-])
+  (if (empty? sorted-seq) 
+    (list n)
+    (do
+      (loop [counter 0
+             orig-seq sorted-seq 
+             tmp-seq [] ]
+        (if (and (not (empty? orig-seq))(> n (first orig-seq)))
+          (recur (inc counter) (rest orig-seq) (conj tmp-seq (first orig-seq)))
+          (concat tmp-seq (conj orig-seq n)))))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (do 
+      (reduce insert [] a-seq))))
 
 (defn parity [a-seq]
   [:-])
