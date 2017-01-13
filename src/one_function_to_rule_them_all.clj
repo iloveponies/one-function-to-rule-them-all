6bdff975cc3f6ef25baa433504486df376842b1c
Mikko Pyykkö <mipyykko@gmail.com>
2017-01-13 21:32:49 +0200

    all but encore done!

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,43 +1,63 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (cond
+    (empty? a-seq) ""
+    :else (reduce (fn [p n] (str p " " n)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (rest (reduce #(conj %1 x %2) [] a-seq)))
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [count e]
+                 (inc count))]
+  (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce #(conj %1 %2) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (vector (apply min a-seq) (apply max a-seq)))
 
-(defn insert [sorted-seq n]
-  [:-])
+(defn insert [sorted n]
+  (loop [s []
+        sd sorted]
+  (cond
+    (empty? sd) (conj s n)
+    (> (first sd) n) (flatten (conj s n sd))
+    :else (recur (conj s (first sd)) (rest sd)))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert [] a-seq))
 
-(defn parity [a-seq]
-  [:-])
+(defn parity [seq]
+  (let [cont (fn [s elem]
+          (cond
+            (some #(= elem %) s) (disj s elem)
+            :else (conj s elem)))]
+  (reduce cont #{} seq)))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- 0 x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
 
-(defn my-* [x]
-  :-)
+(defn count-params [& args]
+  (count args))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn my-*
+  ([& args]
+   (if (zero? (count args)) 1 (reduce * args))))
+
+(defn pred-and
+  ([] (constantly true))
+  ([x] x)
+  ([x y] #(and (x %) (y %)))
+  ([x y & args]
+   (reduce pred-and (pred-and x y) args)))
 
 (defn my-map [f a-seq]
   [:-])
