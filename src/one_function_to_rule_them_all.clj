a57ad9438b7b51d8b9fc8defabf17b085d01159b
Lingnan Dai <lingnan.d@gmail.com>
2017-01-23 14:28:22 +0000

    first parts

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,28 +1,37 @@
-(ns one-function-to-rule-them-all)
+(ns nightcoders.one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat [] a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce (fn [z a] (str z " " a)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (reduce (fn [z a] (concat z [x a])) [] a-seq))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce inc 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [z a] (cons a z)) [] a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [[mn mx] a] 
+            [(min mn a) (max mx a)])
+    [(first a-seq) (first a-seq)] (rest a-seq))) 
 
 (defn insert [sorted-seq n]
-  [:-])
+  (loop [acc []
+         sq sorted-seq]
+    (cond
+      (empty? sq) (reverse (cons n acc))
+      (>= (first sq) n) (concat (reverse acc) (cons n sq))
+      :else (recur (cons (first sq) acc) (rest sq)))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert [] a-seq))
 
 (defn parity [a-seq]
   [:-])
