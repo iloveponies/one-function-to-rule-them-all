af60e590840f7ff586d69281cf30854db84c41fa
Peter Goetsch <pgoetsch@dminc.com>
2017-05-05 23:30:56 +0300

    checkpoint

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -20,18 +20,42 @@
 
 (defn my-reverse [a-seq]
     (let [reducer-fn (fn [current-accum-val next-item]
-                          (assoc current-accum-val 0 next-item))]
-                          ; can shift by 1 for sizeof(a-seq)
-    (reduce reducer-fn '[] a-seq)))
+                          (conj current-accum-val next-item))] ; conj adds to the front of a list
+    (reduce reducer-fn '() a-seq)))
 
 (defn min-max-element [a-seq]
-  [:-])
+    (let [reducer-fn (fn [current-accum-val next-item]
+                          (let [[min-val max-val] current-accum-val]
+                              (cond
+                                (< next-item min-val max-val) (assoc current-accum-val 0 next-item) ; found new min
+                                (< min-val next-item max-val) current-accum-val ; nothing to do here
+                                :else (assoc current-accum-val 1 next-item)))) ; found new max
+            first-item (first a-seq)
+            second-item (first (rest a-seq))]
+            (if (= 1 (count a-seq))
+                (vector (first a-seq) (first a-seq))
+                (if (< first-item second-item)
+                    (reduce reducer-fn [first-item second-item] (rest (rest a-seq)))
+                    (reduce reducer-fn [second-item first-item] (rest (rest a-seq)))))))
+
 
 (defn insert [sorted-seq n]
-  [:-])
+  (if (empty? sorted-seq)
+    (cons n '[])
+    (if (< n (first sorted-seq))
+        (concat [n] sorted-seq)
+        (loop [top-bun (cons (first sorted-seq) '[])
+           bottom-bun (rest sorted-seq)]
+           (if (empty? bottom-bun)
+               (concat top-bun [n]) ; no bottom bun, result is top-bun + n
+               (if (< n (first bottom-bun))
+                   (concat (concat top-bun [n]) bottom-bun) ; make the sandwich with n in the middle
+                   (recur (concat top-bun [(first bottom-bun)]) (rest bottom-bun)))))))) ; move one from buttom bun to top bun
 
 (defn insertion-sort [a-seq]
-  [:-])
+    (let [reducer-fn (fn [current-accum-val next-item]
+                          (insert current-accum-val next-item))]
+    (reduce reducer-fn '() a-seq)))
 
 (defn parity [a-seq]
   [:-])
